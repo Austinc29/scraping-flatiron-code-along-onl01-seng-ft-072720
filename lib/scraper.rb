fb76225891c974fa1b5ed9375e809f3ebbb1910f
Austin Cummings <aac729@live.com>
2020-08-18 22:01:07 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,19 +4,7 @@ require 'pry'
 
 require_relative './course.rb'
 
-class Scraper
- 
-  def get_page
- 
-    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
- 
-  end
- 
-end
 
-def get_courses
-  self.get_page.css(".post")
-end
 
  
 
